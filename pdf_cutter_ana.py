6123569619e3fee77d4ee10d246ff8c2bc5c15a4
adrianhieber <adrian.hieber@gmail.com>
2022-11-29 11:34:23 +0100

    add pdf_cutter

---
+++ pdf_cutter_ana.py
@@ -0,0 +1,13 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+name="Skript-Analysis_III.pdf"#input("Name: ")+".pdf"
+pages=int(input("seiten weglassen: "))
+
+inputpdf = PdfFileReader(open(name, "rb"))
+
+output = PdfFileWriter()
+
+for i in range(pages, inputpdf.numPages):
+    output.addPage(inputpdf.getPage(i))
+    
+with open(f"{name.rstrip('.pdf')}_teiler.pdf", "wb") as outputStream:
+        output.write(outputStream)
